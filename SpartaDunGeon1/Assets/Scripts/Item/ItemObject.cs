ef628dcc6739d6439dcdc80c1c235e34103907e0
유경 <saa1007@naver.com>
2025-08-12 19:49:06 +0900

    아이템 스폰 추가

--- SpartaDunGeon1/Assets/Scripts/Item/ItemObject.cs
+++ SpartaDunGeon1/Assets/Scripts/Item/ItemObject.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public interface IInteractable
 {
@@ -10,6 +11,20 @@ public class ItemObject : MonoBehaviour, IInteractable
 {
     public ItemData data;
 
+    [Header("Respawn")]
+    public float respawnDelay = 10f;
+    public GameObject visualsRoot;
+
+    Vector3 spawnPos;
+    Quaternion spawnRot;
+
+    void Awake()
+    {
+        if (visualsRoot == null) visualsRoot = gameObject;
+        spawnPos = transform.position;
+        spawnRot = transform.rotation;
+    }
+
     public string GetInteractPrompt()
     {
         string str = $"{data.displayName}\n{data.description}";
@@ -20,6 +35,23 @@ public class ItemObject : MonoBehaviour, IInteractable
     {
         CharacterManager.Instance.Player.itemData = data;
         CharacterManager.Instance.Player.addItem?.Invoke();
-        Destroy(gameObject);
+
+        SetVisible(false);
+        Invoke(nameof(Respawn), respawnDelay);
+    }
+    void Respawn()
+    {
+        // 원위치/자세로 돌려놓고 다시 보이기
+        transform.SetPositionAndRotation(spawnPos, spawnRot);
+        var rb = GetComponent<Rigidbody>();
+        if (rb) { rb.velocity = Vector3.zero; rb.angularVelocity = Vector3.zero; }
+
+        SetVisible(true);
+    }
+
+    void SetVisible(bool on)
+    {
+        foreach (var r in visualsRoot.GetComponentsInChildren<Renderer>(true)) r.enabled = on;
+        foreach (var c in visualsRoot.GetComponentsInChildren<Collider>(true)) c.enabled = on;
     }
 }
